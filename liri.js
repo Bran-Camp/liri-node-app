37e9aaee0a1db5303f2b688132abe78c0b0fe0c6
Brandon Campbell <Brandon Campbell>
2017-04-23 20:52:21 -0500

    spotify, twitter, omdb updates

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var keys = require('./keys.js');
 
 var twitModule = require('twitter');
 var spotModule = require('spotify');
+var reqModule = require('request');
 
 var twitClient = new twitModule(keys.twitterKeys);
 
@@ -14,6 +15,7 @@ switch(process.argv[2]){
 	case "my-tweets": 
 		twitClient.get('statuses/user_timeline', twitParams, tCallBack);
 		break;
+
 	case "spotify-this-song":
 		//console.log(process.argv.length);
 		if (process.argv.length < 4) {
@@ -32,8 +34,18 @@ switch(process.argv[2]){
 		
 		break;
 
-}
+	case "movie-this":
+		var reqParams = {
+			url: 'http://www.omdbapi.com/?t=' + process.argv[3] + '&plot=short&tomatoes=true'
+		}
+		reqModule(reqParams, omdbSearch);
+		break;
 
+	default:
+    	console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this}");
+  		break;
+
+}
 
 function tCallBack(err, twts, res) {
 	
@@ -43,6 +55,10 @@ function tCallBack(err, twts, res) {
 	}
 	
 }
+
+function twtTxt(eachTweet) {
+		console.log("Tweet:" + " " + eachTweet.text + " Created at:" + " " + eachTweet.created_at.substring(0, 19));
+	}
 
 function spotCallBack(err, data) {
 	//console.log(err);
@@ -55,6 +71,9 @@ function spotLookUp(err, data) {
 	console.log("Artist: " + data.artists[0].name + "\r\nSong title: " + data.name + " \r\nAlbum: " + data.album.name + "\r\nSong link: " + data.preview_url);	
 }
 
-function twtTxt(eachTweet) {
-		console.log("Tweet:" + " " + eachTweet.text + " Created at:" + " " + eachTweet.created_at.substring(0, 19));
+function omdbSearch(err, response, body) {
+	if (!err && response.statusCode == 200) {
+	    var info = JSON.parse(body);
+	    console.log("Title: " + info.Title +"\r\nRelease Year: " + info.Year + "\r\nIMDB Rating: " + info.imdbRating + "\r\nCountry: " + info.Country + "\r\nLanguage: " + info.Language + "\r\nPlot: " + info.Plot + "\r\nActors: " + info.Actors + "\r\nRotten Tomatoes Rating: " + info.tomatoRating + "\r\nRotten Tomatoes URL: " + info.tomatoURL);
 	}
+}
